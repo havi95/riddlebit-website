5a6f9fb268348aee5975cd7994c06f60270f8916
Håvard Aasmo <havi95@gmail.com>
2018-09-20 00:09:06 +0200

    Update MemberCard to load default image over HTTPS

--- src/components/Content/About/OurTeam/MemberCard/MemberCard.js
+++ src/components/Content/About/OurTeam/MemberCard/MemberCard.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './MemberCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const MemberCard = ({name, description, imageURL, twitter, mail}) => (
-    <div className='member-card'>
-        <img src={imageURL} alt={[name, description].join(' ')} />
-        <div className="card-content">
-            <h3>{name}</h3>
-            <p>{description}</p>
-            <div className="card-social">
-                {twitter && (
-                    <a href={twitter}><FontAwesomeIcon className='card-icon' icon={['fab', 'twitter']} /></a>
-                )}
-                {mail && (
-                    <a href={'mailto:'+mail}><FontAwesomeIcon className='card-icon' icon={['fa', 'envelope']} /></a>
-                )}
+class MemberCard extends Component {
+
+    renderPicture() {
+        if (this.props.imageURL) {
+            return (
+                <img src={this.props.imageURL} alt={[this.props.name, this.props.description].join(' ')} />
+            );
+        } else {
+            return (
+                <img src='https://i.imgur.com/V9QJQSj.png' alt={[this.props.name, this.props.description].join(' ')} />
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div className='member-card'>
+                {this.renderPicture()}
+                <div className="card-content">
+                    <h3>{this.props.name}</h3>
+                    <p>{this.props.description}</p>
+                    <div className="card-social">
+                        {this.props.twitter && (
+                            <a href={this.props.twitter}><FontAwesomeIcon className='card-icon' icon={['fab', 'twitter']} /></a>
+                        )}
+                        {this.props.mail && (
+                            <a href={'mailto:'+this.props.mail}><FontAwesomeIcon className='card-icon' icon={['fa', 'envelope']} /></a>
+                        )}
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 export default MemberCard;
--- src/components/Content/About/OurTeam/OurTeam.js
+++ src/components/Content/About/OurTeam/OurTeam.js
@@ -7,27 +7,22 @@ const OurTeam = () => (
         <h1>Our Team</h1>
         <div className='member-cards'>
             <MemberCard
-                imageURL='http://www.tuktukdesign.com/wp-content/plugins/all-in-one-seo-pack-pro/images/default-user-image.png'
                 name='Jonathan Jørgensen'
                 description='Co-Founder & Developer'
             />
             <MemberCard
-                imageURL='http://www.tuktukdesign.com/wp-content/plugins/all-in-one-seo-pack-pro/images/default-user-image.png'
                 name='Vegard Farstad'
                 description='Co-Founder & Developer'
             />
             <MemberCard
-                imageURL='http://www.tuktukdesign.com/wp-content/plugins/all-in-one-seo-pack-pro/images/default-user-image.png'
                 name='Oliver Nakken'
                 description='Co-Founder & Developer'
             />
             <MemberCard
-                imageURL='http://www.tuktukdesign.com/wp-content/plugins/all-in-one-seo-pack-pro/images/default-user-image.png'
                 name='Håvard Aasmo'
                 description='Co-Founder & Developer'
             />
             <MemberCard
-                imageURL='http://www.tuktukdesign.com/wp-content/plugins/all-in-one-seo-pack-pro/images/default-user-image.png'
                 name='Lars Olav Muri'
                 description='Chairman & Accountant'
             />
