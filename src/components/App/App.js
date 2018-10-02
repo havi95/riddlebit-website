02bb7f62bf53ce76322aa08afcf7552382838068
Håvard Aasmo <havi95@gmail.com>
2018-10-03 00:39:51 +0200

    Add simple footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import Content from '../Content/Content';
+import Footer from '../Footer/Footer';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope, faAngleDown, faChevronDown, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
-library.add(faTwitter, faFacebook, faEnvelope, faAngleDown, faChevronDown, faBars, faTimes);
+import { faTwitter, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+library.add(faTwitter, faFacebook, faGithub, faEnvelope, faAngleDown, faChevronDown, faBars, faTimes);
 
 class App extends Component {
     render() {
@@ -16,6 +17,7 @@ class App extends Component {
             <div className="App">
                 <Navbar />
                 <Content />
+                <Footer />
             </div>
             </BrowserRouter>
         )
