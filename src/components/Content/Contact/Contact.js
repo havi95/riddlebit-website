6c38a7bce919c749812b1dc42c9a6678a38d8952
Håvard Aasmo <havi95@gmail.com>
2018-10-23 16:24:35 +0200

    Install emailjs & add contact form functionality

--- src/components/Content/Contact/Contact.js
+++ src/components/Content/Contact/Contact.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import './Contact.css';
 import ContactForm from './ContactForm/ContactForm';
+import * as emailjs from 'emailjs-com';
 
 class Contact extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            messageSent: false
+        };
+        this.sendMessage = this.sendMessage.bind(this);
+    }
+
+    sendMessage(name, email, message, clearForm) {
+
+        const params = {
+            name: name,
+            email: email,
+            message: message
+        };
+
+        emailjs.send('mailgun', 'riddlebit_contact', params, process.env.REACT_APP_EMAILJS_USERID)
+        .then((res) => {
+            console.log(res)
+            this.setState({
+                messageSent: true
+            });
+            clearForm();
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
     render() {
         return (
             <div id='contact' className='content-section contact-content'>
                 <h1>Contact</h1>
-                <ContactForm />
+                <p>
+                    Do you have a question? Or maybe you just want to give some feedback?<br/>
+                    Feel free to contact us in english or norwegian.
+                </p>
+                {this.state.messageSent &&
+                    <h4 className='sent-alert'>Message was sent</h4>
+                }
+                <ContactForm callback={this.sendMessage} />
             </div>
         )
     }
--- src/components/Content/Contact/ContactForm/ContactForm.js
+++ src/components/Content/Contact/ContactForm/ContactForm.js
@@ -1,18 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ContactForm.css';
 
-const ContactForm = () => (
-    <form className='contact-form'>
-        <label htmlFor='name'>Name</label>
-        <input className='input-field' type='text' name='name' id='name' placeholder='Your Name' />
-        <label htmlFor='email'>Email</label>
-        <input className='input-field' type='email' name='email' id='email' placeholder='Your Email' />
-        <label htmlFor='message'>Message</label>
-        <textarea className='message-field' rows='8' name='message' id='message' />
-        <div className="button-group">
-            <input className='button' type='submit' value="Send" />
-        </div>
-    </form>
-);
+class ContactForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.clearForm = this.clearForm.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let name = this.state.name;
+        let email = this.state.email;
+        let message = this.state.message;
+        if (name && email && message) {
+            this.props.callback(name, email, message, this.clearForm);
+        }
+    }
+
+    clearForm() {
+        this.setState({
+            name: '',
+            email: '',
+            message: ''
+        });
+    }
+
+    render() {
+        return(
+            <form className='contact-form' onSubmit={this.handleSubmit}>
+                <label htmlFor='name'>Name</label>
+                <input  onChange={e => this.setState({name: e.target.value})}
+                        value={this.state.name}
+                        className='input-field'
+                        type='text' name='name' id='name'
+                        placeholder='Your Name' required />
+                <label htmlFor='email'>Email</label>
+                <input  onChange={e => this.setState({email: e.target.value})}
+                        value={this.state.email}
+                        className='input-field'
+                        type='email'
+                        name='email' id='email'
+                        placeholder='Your Email' required />
+                <label htmlFor='message'>Message</label>
+                <textarea   onChange={e => this.setState({message: e.target.value})}
+                            value={this.state.message}
+                            className='message-field' rows='8'
+                            name='message' id='message' required />
+                <div className="button-group">
+                    <input className='button' type='submit' value="Send" />
+                </div>
+            </form>
+        )
+    }
+
+}
 
 export default ContactForm;
