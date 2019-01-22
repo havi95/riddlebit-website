0403b74f95175de60969758efeb314784fe3a040
Håvard Aasmo <havi95@gmail.com>
2019-01-22 16:00:55 +0100

    Update 'About' content

--- src/components/Content/Frontpage/About/About.js
+++ src/components/Content/Frontpage/About/About.js
@@ -5,15 +5,15 @@ const About = () => (
         <div className='content-section content-width-md'>
                 <h1>We Make Games</h1>
                 <p>     
-                    Riddlebit Software was founded late in 2017.
-                    But it all really started in February 2018,
-                    when we won two awards at Norwegian Game Awards.
+                    Riddlebit Software was founded in late 2017.
+                    However, it all really started in February 2018
+                    when we won two awards at <a href="https://gameawards.no/">Norwegian Game Awards</a>.
                     Both awards were for our current project "Setback",
                     which had only been in development for a month.
                     That was a big deal for us as a company because it showed we had a
                     solid proof of concept that we could continue to work on.
-                    Since then, we have attended events such as Torucon and SpillExpo with a multiplayer demo of Setback.
-                    Both of which was a really good experience for us.
+                    Since then, we have attended events such as <a href="https://torucon.no/en/home/">Torucon</a> and <a href="https://www.spillexpo.no/">SpillExpo</a> with
+                    a multiplayer demo of Setback. Both of which were a really good experience for us.
                 </p>
         </div>
     </div>
