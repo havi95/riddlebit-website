e183799a0e854c2f8bea69e1bcf58b0c9a3d0b5a
Håvard Aasmo <havi95@gmail.com>
2019-05-23 01:43:07 +0200

    Uninstall react-cookie package

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import { CookiesProvider, Cookies, withCookies } from 'react-cookie';
-import { instanceOf } from 'prop-types';
 import Navbar from '../Navbar';
 import Content from '../Content';
 import Footer from '../Footer';
@@ -12,45 +10,16 @@ import { faEnvelope, faAngleDown, faChevronDown, faBars, faTimes } from '@fortaw
 import { faTwitter, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
 library.add(faTwitter, faFacebook, faGithub, faEnvelope, faAngleDown, faChevronDown, faBars, faTimes);
 
-class App extends Component {
-
-    static propTypes = {
-        cookies: instanceOf(Cookies).isRequired
-    };
-
-    constructor(props) {
-        super(props);
-        const { cookies } = props;
-        this.state = {
-            consented: cookies.get('consented') || false
-        };
-        this.consent = this.consent.bind(this);
-    }
-
-    consent() {
-        const { cookies } = this.props;
-        let expires = new Date();
-        expires.setMonth(expires.getMonth() + 6);
-        cookies.set('consented', true, {expires: expires});
-        this.setState({
-            consented: true
-        });
-        window.location.reload();
-    }
-
-    render() {
-        return (
-            <CookiesProvider>
-            <BrowserRouter>
-            <div className="App">
-                <Navbar />
-                <Content />
-                <Footer />
-            </div>
-            </BrowserRouter>
-            </CookiesProvider>
-        )
-    }
+const App = () => {
+    return (
+        <BrowserRouter>
+        <div className="App">
+            <Navbar />
+            <Content />
+            <Footer />
+        </div>
+        </BrowserRouter>
+    )
 }
 
-export default withCookies(App);
+export default App;
