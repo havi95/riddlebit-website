dcb765c96aabc1329163f96bc093b57238d07cbd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-24 21:48:00 +0000

    Deploy www.riddlebit.net to github.com/Riddlebit/riddlebit-website.git:gh-pages

---
+++ precache-manifest.f2678d8a16a7628ea98a4f6890f1b7e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e255a8b3cc028933199438fd767eec3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68d505a3994e01601055",
+    "url": "/static/css/main.cf7c3eaa.chunk.css"
+  },
+  {
+    "revision": "467c41f2fd7f263d4181",
+    "url": "/static/js/2.d9be305c.chunk.js"
+  },
+  {
+    "revision": "68d505a3994e01601055",
+    "url": "/static/js/main.f516437d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d1c2507db07eeeac033f57faf2160a9a",
+    "url": "/static/media/logo.d1c2507d.svg"
+  }
+]);
